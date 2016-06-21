e1029459c8ba9975cb04de40cf4a473fc61d7101
yuichi.watanabe <yuichi.watanabe.ja@gmail.com>
2016-06-21 22:34:54 +0900

    update d.ts

--- dist/botbuilder-redis.d.ts
+++ dist/botbuilder-redis.d.ts
@@ -2,7 +2,7 @@ import redis = require("redis");
 import botbuilder = require("botbuilder");
 declare class RedisStorage implements botbuilder.IStorage {
     private client;
-    constructor(client: redis.RedisClient);
+    constructor(client: redis.RedisClient, prefix?: string);
     get(id: string, callback: (err: Error, data: any) => void): void;
     save(id: string, data: any, callback?: (err: Error) => void): void;
     delete(id: string, callback?: (err: Error) => void): void;
