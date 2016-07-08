1bea0784c0784b2dc6b6b98f5cd053c4610a0f75
yuichi.watanabe <yuichi.watanabe.ja@gmail.com>
2016-07-09 08:26:58 +0900

    add default keyword to d.ts

--- dist/botbuilder-redis.d.ts
+++ dist/botbuilder-redis.d.ts
@@ -1,10 +1,11 @@
-import redis = require("redis");
-import botbuilder = require("botbuilder");
-declare class RedisStorage implements botbuilder.IStorage {
-    private client;
+declare module 'botbuilder-redis' {
+  import * as redis from 'redis';
+  import * as botbuilder from 'botbuilder';
+  export default class RedisStorage implements botbuilder.IStorage {
+    private client: redis.RedisClient;
     constructor(client: redis.RedisClient, prefix?: string);
     get(id: string, callback: (err: Error, data: any) => void): void;
     save(id: string, data: any, callback?: (err: Error) => void): void;
     delete(id: string, callback?: (err: Error) => void): void;
+  }
 }
-export = RedisStorage;
